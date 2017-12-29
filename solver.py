eb6f51f55bdbfb32945b56f53cac4b9268784c00
William Bain <bain.william.a@gmail.com>
2017-12-28 21:38:10 -0500

    Optimization: Prune partial value assignments
    Instead of generating all candidate assignments and then pruning
    out invalid ones, heuristically detect partial assignments which
    have become non-viable and don't generate further candidates.
    
    This makes the solver consistently run in under a second.

--- solver.py
+++ solver.py
@@ -68,6 +68,8 @@ def generate_bounded(entries, cons, start_idx):
         if entries[i] is not None:
             continue
 
+        bounded = []
+
         # Restriction: true
         v_true = list(entries)
         v_true[i] = True
@@ -76,8 +78,15 @@ def generate_bounded(entries, cons, start_idx):
         v_false = list(entries)
         v_false[i] = False
 
-        return generate_bounded(v_true, cons, i + 1) + \
-            generate_bounded(v_false, cons, i + 1)
+        if is_pruneable(v_true, cons, i + 1):
+            return generate_bounded(v_false, cons, i + 1)
+
+        bounded = generate_bounded(v_true, cons, i + 1)
+
+        if not is_pruneable(v_false, cons, i + 1):
+            bounded += generate_bounded(v_false, cons, i + 1)
+
+        return bounded
 
     # Base case: fully assigned
     if is_consistent(entries, cons):
@@ -86,23 +95,65 @@ def generate_bounded(entries, cons, start_idx):
     return []
 
 
+def is_pruneable(values, cons, end_idx):
+    """Heuristically determine if this partial solution can be pruned
+
+    If the value assignment up to end_idx cannot possibly satisfy cons, then
+    we can throw this assignment away.
+    """
+    assert all(isinstance(v, bool) for v in values[:end_idx])
+    runs = get_runs(values, end_idx=end_idx)
+
+    if not runs:
+        return False
+
+    if len(runs) > len(cons):
+        return True
+
+    # Handle scenario where final run may be extended by further assignments
+    if runs[-1] < cons[len(runs) - 1] and \
+        final_run_could_be_extended(values, end_idx):
+        del runs[-1]
+
+    return runs != cons[:len(runs)]
+
+
+def final_run_could_be_extended(values, end_idx):
+    """Heuristically see if the value run at end_idx could be extended
+
+    Returns true if there is a run at end_idx, if end_idx is not the last
+    value, and if the value after end_idx could be part of a run.
+
+    To keep this constant-time, we aren't checking if the run can actually
+    be extended to the desired length.
+    """
+    if end_idx == 0 or end_idx == len(values):
+        return False
+
+    return values[end_idx - 1] is True and values[end_idx] is not False
+
+
 def is_consistent(values, cons):
-    """Values are consistent with constraints if the shortest
-    conceivable runs are *** and the longest are ***"""
+    """Determine if a value assignment is consistent with the constraints
+
+    Values are consistent with constraints if their runs have the
+    lengths given in the constraints"""
     assert all(isinstance(v, bool) for v in values)
     return get_runs(values) == cons
 
 
-def get_runs(values):
+def get_runs(values, end_idx=None):
     runs = []
     i = 0
-    while i < len(values):
+    if end_idx is None:
+        end_idx = len(values)
+    while i < end_idx:
         if not values[i]:
             i += 1
             continue
 
         run_start = i
-        while i < len(values) and values[i]:
+        while i < end_idx and values[i]:
             i += 1
         runs.append(i - run_start)
     return runs
